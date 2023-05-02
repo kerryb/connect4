c614743cc8bef7e124a31400a2a95c9fc60015ba
Kerry Buckley <kerryjbuckley@gmail.com>
2023-05-02 09:41:52 +0100

    Add typespecs

--- lib/connect4/game/game.ex
+++ lib/connect4/game/game.ex
@@ -1,16 +1,23 @@
 defmodule Connect4.Game do
   use GenServer
 
+  @enforce_keys [:state]
   defstruct [:state]
 
+  @type state :: :player_1_to_play
+  @type t :: %__MODULE__{state: state()}
+
+  @spec start_link(any()) :: GenServer.on_start()
   def start_link(_arg) do
     GenServer.start_link(__MODULE__, nil)
   end
 
+  @spec new :: t()
   def new do
     %__MODULE__{state: :player_1_to_play}
   end
 
+  @spec state(GenServer.server()) :: state()
   def state(game), do: GenServer.call(game, :state)
 
   @impl GenServer
