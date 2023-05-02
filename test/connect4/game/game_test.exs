881d0e12b163615e2953ef3154383a20f0fd3198
Kerry Buckley <kerryjbuckley@gmail.com>
2023-05-02 17:48:23 +0100

    Extract test helpers for playing moves

--- test/connect4/game/game_test.exs
+++ test/connect4/game/game_test.exs
@@ -16,90 +16,61 @@ defmodule Connect4.GameTest do
     end
 
     test "alternates players’ turns" do
-      {:ok, _} = Game.play(@game_id, :O, 0)
+      {:ok, _} = play_move(:O, 0)
       assert Game.next_player(@game_id) == :X
-      {:ok, _} = Game.play(@game_id, :X, 0)
+      {:ok, _} = play_move(:X, 0)
       assert Game.next_player(@game_id) == :O
     end
 
     test "keeps track of moves" do
-      {:ok, _} = Game.play(@game_id, :O, 3)
-      {:ok, _} = Game.play(@game_id, :X, 2)
-      {:ok, state} = Game.play(@game_id, :O, 2)
+      play_moves(O: 3, X: 2)
+      {:ok, state} = play_move(:O, 2)
       assert state.board == %{2 => %{0 => :X, 1 => :O}, 3 => %{0 => :O}}
     end
 
     test "detects four in a row horizontally as a win" do
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 2)
-      {:ok, _} = Game.play(@game_id, :O, 3)
-      {:ok, _} = Game.play(@game_id, :X, 3)
-      {:ok, _} = Game.play(@game_id, :O, 0)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, state} = Game.play(@game_id, :O, 1)
+      state = play_moves(O: 2, X: 2, O: 3, X: 3, O: 0, X: 0, O: 1)
       assert state.winner == :O
     end
 
     test "detects four in a row vertically as a win" do
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, state} = Game.play(@game_id, :O, 2)
+      state = play_moves(O: 2, X: 0, O: 2, X: 0, O: 2, X: 0, O: 2)
       assert state.winner == :O
     end
 
     test "detects four in a row diagonally to the left as a win" do
-      {:ok, _} = Game.play(@game_id, :O, 3)
-      {:ok, _} = Game.play(@game_id, :X, 2)
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 1)
-      {:ok, _} = Game.play(@game_id, :O, 1)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, _} = Game.play(@game_id, :O, 1)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, _} = Game.play(@game_id, :O, 0)
-      {:ok, _} = Game.play(@game_id, :X, 4)
-      {:ok, state} = Game.play(@game_id, :O, 0)
+      state = play_moves(O: 3, X: 2, O: 2, X: 1, O: 1, X: 0, O: 1, X: 0, O: 0, X: 4, O: 0)
       assert state.winner == :O
     end
 
     test "detects four in a row diagonally to the right as a win" do
-      {:ok, _} = Game.play(@game_id, :O, 0)
-      {:ok, _} = Game.play(@game_id, :X, 1)
-      {:ok, _} = Game.play(@game_id, :O, 1)
-      {:ok, _} = Game.play(@game_id, :X, 2)
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 3)
-      {:ok, _} = Game.play(@game_id, :O, 2)
-      {:ok, _} = Game.play(@game_id, :X, 3)
-      {:ok, _} = Game.play(@game_id, :O, 3)
-      {:ok, _} = Game.play(@game_id, :X, 6)
-      {:ok, state} = Game.play(@game_id, :O, 3)
+      state = play_moves(O: 0, X: 1, O: 1, X: 2, O: 2, X: 3, O: 2, X: 3, O: 3, X: 6, O: 3)
       assert state.winner == :O
     end
 
     test "does not allow play out of turn" do
-      assert {:error, "Not your turn"} = Game.play(@game_id, :X, 0)
+      assert {:error, "Not your turn"} = play_move(:X, 0)
     end
 
     test "does not allow play in an invalid column" do
-      assert {:error, "Column must be 0..6"} = Game.play(@game_id, :O, -1)
-      assert {:error, "Column must be 0..6"} = Game.play(@game_id, :O, 7)
-      assert {:error, "Column must be 0..6"} = Game.play(@game_id, :O, "foo")
+      assert {:error, "Column must be 0..6"} = play_move(:O, -1)
+      assert {:error, "Column must be 0..6"} = play_move(:O, 7)
+      assert {:error, "Column must be 0..6"} = play_move(:O, "foo")
     end
 
     test "does not allow play in a full column" do
-      {:ok, _} = Game.play(@game_id, :O, 0)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, _} = Game.play(@game_id, :O, 0)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      {:ok, _} = Game.play(@game_id, :O, 0)
-      {:ok, _} = Game.play(@game_id, :X, 0)
-      assert {:error, "Column is full"} = Game.play(@game_id, :O, 0)
+      play_moves(O: 0, X: 0, O: 0, X: 0, O: 0, X: 0)
+      assert {:error, "Column is full"} = play_move(:O, 0)
     end
+
+    defp play_moves(moves) do
+      Enum.reduce(moves, nil, fn {player, column}, _ ->
+        {:ok, state} = play_move(player, column)
+        state
+      end)
+    end
+
+    defp play_move(player, column), do: Game.play(@game_id, player, column)
   end
 
   describe "Inspect implementation for Connect4.Game" do
