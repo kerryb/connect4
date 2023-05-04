8d16a7d0872c857f13692c75f6dc8f8bc7826ac7
Kerry Buckley <kerryjbuckley@gmail.com>
2023-05-04 07:57:10 +0100

    Upgrade esbuild to 0.17.18

--- config/config.exs
+++ config/config.exs
@@ -31,7 +31,7 @@ config :connect4, Connect4.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.11",
+  version: "0.17.18",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
